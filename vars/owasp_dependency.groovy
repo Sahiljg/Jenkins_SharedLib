2689e14e59b7b34725fbc195e40dd6d8916868ea
Sahiljg <113673505+Sahiljg@users.noreply.github.com>
2026-02-04 12:54:35 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,11 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+        dependencyCheck additionalArguments: """
+            --nvdApiKey ${NVD_API_KEY}
+            --format HTML
+            --format XML
+        """, odcInstallation: 'DP-Check'
+        
+        dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+    }
 }
